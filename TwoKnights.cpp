e51ee51a7519db74a54aa4551074e0849c14caf2
gvarys <johnnyzaev@gmail.com>
2023-01-05 20:23:10 +0600

    TwoKnights done!

--- TwoKnights.cpp
+++ TwoKnights.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-
+	long long n;
+	cin >> n;
+	long long totalCombs, notValidComps;
+	for (long long i = 1; i <= n; i++)
+	{
+		totalCombs = ((i * i) * ((i * i) - 1)) / 2;
+		notValidComps = 4 * (i - 1) * (i - 2);
+		cout << totalCombs - notValidComps << endl;
+	}
 }
